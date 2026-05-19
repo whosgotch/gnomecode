600ec3d241d8bd1c74566aa63e65fdefd470133b
Artem Komov <komovaar@gmail.com>
2026-05-19 14:59:59 +0300

    feat: add initial chat loop

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gnomecode
+
+go 1.26.3
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("you> ")
+
+		msg, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+
+		msg = strings.TrimSpace(msg)
+		if msg == "" {
+			continue
+		}
+
+		if msg == ":quit" || msg == "exit" {
+			break
+		}
+
+		fmt.Println("Nice!")
+	}
+}
